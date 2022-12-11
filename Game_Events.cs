bc7b549a91c83059d62f082298222d46d85f6221
NightHuanya <47856051+NightHuanya@users.noreply.github.com>
2022-12-11 19:19:53 +0800

    Upload

---
+++ Game_Events.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game_Events : MonoBehaviour
+{
+    public Cool_Down_List cool_Down_List;
+    private void Start()
+    {
+        cool_Down_List = GameObject.FindWithTag("Game_Manager").GetComponent<Cool_Down_List>();
+        player_Scrpit = GameObject.FindWithTag("Player").GetComponent<Player>();
+    }
+
+    public void Call_Skill(string Method_Nmae,string Skill_ID)
+    {
+            StartCoroutine(Method_Nmae,Skill_ID);
+    }
+    public void Call_Potion(string Method_Nmae, Item Potion)
+    {
+        StartCoroutine(Method_Nmae,Potion);
+    }
+    public void Call_SystemKey(string Method_Nmae)
+    {
+        for (int i = 0; i < cool_Down_List.Cool_Downs.Count; i++)
+        {
+            if (cool_Down_List.Cool_Downs[i].Name == Method_Nmae)
+            {
+                return;
+            }
+        }
+            StartCoroutine(Method_Nmae);
+    }
+
+    public GameObject KeyBoard_UI;
+    IEnumerator Open_KeyBoard_Setting()
+    {
+        cool_Down_List.Add_Cool_Down_Event("Open_KeyBoard_Setting", 0.3f);
+        KeyBoard_UI.SetActive(!KeyBoard_UI.activeSelf);
+        yield return null;
+    }
+
+    public GameObject Backpack_UI;
+    IEnumerator Open_Backpack()
+    {
+        cool_Down_List.Add_Cool_Down_Event("Open_Backpack", 0.3f);
+        Backpack_UI.SetActive(!Backpack_UI.activeSelf);
+        yield return null;
+    }
+
+    public GameObject Skill_UI;
+    IEnumerator Open_Skill()
+    {
+        cool_Down_List.Add_Cool_Down_Event("Open_Skill", 0.3f);
+        Skill_UI.SetActive(!Skill_UI.activeSelf);
+        yield return null;
+    }
+
+    public GameObject EquipmentInventory_UI;
+    IEnumerator Open_EquipmentInventory()
+    {
+        cool_Down_List.Add_Cool_Down_Event("Open_EquipmentInventory", 0.3f);
+        EquipmentInventory_UI.SetActive(!EquipmentInventory_UI.activeSelf);
+        yield return null;
+    }
+    
+    public GameObject CharacterStat_UI;
+    IEnumerator Open_CharacterStat()
+    {
+        cool_Down_List.Add_Cool_Down_Event("Open_CharacterStat", 0.3f);
+        CharacterStat_UI.SetActive(!CharacterStat_UI.activeSelf);
+        yield return null;
+    }
+
+    public Player player_Scrpit;
+    IEnumerator Normal_Attack()
+    {
+        cool_Down_List.Add_Cool_Down_Event("Normal_Attack", 0.05f);
+        player_Scrpit.Player_Attack();
+        yield return null;
+    }
+    IEnumerator Jump()
+    {
+        cool_Down_List.Add_Cool_Down_Event("Jump", 0.05f);
+        player_Scrpit.Player_Jump();
+        yield return null;
+    }
+    IEnumerator Pick_Up_Item()
+    {
+        cool_Down_List.Add_Cool_Down_Event("Pick_Up_Item", 0.05f);
+        player_Scrpit.Player_Pick_Up_Item();
+        yield return null;
+    }
+}
---
+++ Keyboard_Setting.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Game_Key
+{
+    public string[] Event_Name = new string[0];
+    public string Key_Name;
+    public Image KeyImage;
+    public Item[] Item_content = new Item[0];
+    public string[] Skill_ID = new string[0];
+}
+public class Keyboard_Setting : MonoBehaviour
+{
+    public GameObject[] Standby_Keys;
+    public Game_Key[] Keys = new Game_Key[54];
+    public float[] Game_Keys_Cool_Down = new float[54];
+    public GameObject Keys_Parent;
+    public Game_Events game_Events;
+
+    void Start()
+    {
+        GameObject[] All_KeysImage = GameObject.FindGameObjectsWithTag("ItemBox_KeyBoard");
+        for (int i = 0; i < All_KeysImage.Length; i++)
+        {
+            Keys[i] = new Game_Key();
+            Keys[i].KeyImage = All_KeysImage[i].GetComponent<Image>();
+            Keys[i].Key_Name = All_KeysImage[i].name.Substring(4);
+            Keys[i].Event_Name = All_KeysImage[i].GetComponent<UI_Drag_Base>().Method_Name;
+        }
+        Standby_Keys = GameObject.FindGameObjectsWithTag("ItemBox_Standby_Keys");
+        game_Events = gameObject.GetComponent<Game_Events>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int Index = 0;
+        foreach ( Game_Key Key in Keys)
+        {
+            if (Input.GetKey(Key.Key_Name.ToLower()))
+            {
+                if(Game_Keys_Cool_Down[Index] >= 0.01f)
+                {
+                    for (int i = 0; i < Key.Event_Name.Length; i++)
+                    {
+                        switch (Key.KeyImage.gameObject.GetComponent<UI_Drag_Base>().Type.ToString())
+                        {
+                            case "Potion":
+                                game_Events.Call_Potion((Key.Event_Name[i]), Key.Item_content[i]);
+                                break;
+                            case "Skill":
+                                game_Events.Call_Skill((Key.Event_Name[i]), Key.Skill_ID[i]);
+                                break;
+                            case "SystemKey":
+                                game_Events.Call_SystemKey((Key.Event_Name[i]));
+                                break;
+                            default:
+                                break;
+                        }
+                    }
+                    Game_Keys_Cool_Down[Index] = 0;
+                }
+                Game_Keys_Cool_Down[Index] += Time.deltaTime;
+                Index++;
+            }
+        }
+    }
+}
